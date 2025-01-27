7c6be43d45f0be62ee406bcc1a6d6663f8f1adee
BCS201003 <bcs201003@gmail.com>
2025-01-27 11:26:07 +0500

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tiktok_tutorial/constants.dart';
 import 'package:tiktok_tutorial/controllers/auth_controller.dart';
 import 'package:tiktok_tutorial/services/firebase_service.dart';
 import 'package:tiktok_tutorial/views/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart'; // Ensure this import is present
 
-void main() {
+Future<void> main() async {
+  // Ensures that plugin services are initialized before Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    // Initialize Firebase asynchronously
+    await Firebase.initializeApp();
+    print('Firebase initialized successfully');
+  } catch (e) {
+    // Handle Firebase initialization errors
+    print('Firebase initialization failed: $e');
+  }
+
+  // Run the Flutter application
   runApp(const MyApp());
 }
 
@@ -17,6 +30,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialBinding: BindingsBuilder(() {
+        // Initialize your controllers and services
         Get.put<AuthController>(AuthController());
         Get.put<FirebaseService>(FirebaseService());
       }),
